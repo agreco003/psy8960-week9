8f1ca7a4c9f6129c6bd2096454032d539539fc92
agreco <greco031@umn.edu>
2023-03-26 18:30:37 -0500

    trying to switch everything to CSS tags

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -7,18 +7,17 @@ library(rvest)
 
 rstats_html <- read_html("https://old.reddit.com/r/rstats/")
 
-post <- rstats_html %>% 
-  html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "outbound", " " ))] | //*[(@id = "thing_t3_11zn9bu")]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))] | //*[(@id = "thing_t3_11zgjfx")]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "self", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))]') %>%
+post <- rstats_html %>%
+  html_elements(css = '.title.outbound , .self .title.may-blank') %>%
   html_text()
 
 upvotes <- rstats_html %>% 
-  html_elements(xpath = '//*[contains(@class, "even") or contains(@class, "odd")]//div[@class = "score unvoted"]') %>%
+  html_elements(css = '.score.unvoted') %>%
   html_text() %>%
-  as.numeric() %>%
-  replace_na(replace = 0)
+  as.numeric()
 
 comments <- rstats_html %>% 
-  html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "comments", " " ))]') %>%
+  html_elements(css = '.first') %>%
   html_text() %>% 
   str_remove(".?comment?.?") %>%
   as.numeric() %>%
@@ -30,7 +29,26 @@ ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
-HTMLcor <- cor.test(x= rstats_tbl$upvotes, rstats_tbl$comments)
+HTMLcor <- cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments)
 
 #Publication
 paste("The correlation between upvotes and comments was r(", HTMLcor$parameter, ") = ", str_remove(round(HTMLcor$estimate, digits = 2), "^0+"),", p = ", str_remove(round(HTMLcor$p.value, digits = 2),"^0+"),". This test was not statistically significant.", sep = "")
+
+
+#XPATH 
+#post <- rstats_html %>% 
+#html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "outbound", " " ))] | //*[(@id = "thing_t3_11zn9bu")]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))] | //*[(@id = "thing_t3_11zgjfx")]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "self", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " )) and contains(concat( " ", @class, " " ), concat( " ", "may-blank", " " ))]') %>%
+#html_text()
+
+#upvotes <- rstats_html %>% 
+  #html_elements(xpath = '//*[contains(@class, "even") or contains(@class, "odd")]//div[@class = "score unvoted"]') %>%
+  #html_text() %>%
+  #as.numeric() %>%
+  #replace_na(replace = 0)
+
+#comments <- rstats_html %>% 
+  #html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "comments", " " ))]') %>%
+  #html_text() %>% 
+  #str_remove(".?comment?.?") %>%
+  #as.numeric() %>%
+  #replace_na(replace = 0)
