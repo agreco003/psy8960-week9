0d8c5e5beeec195f378a3dedf302f9abc56afa2f
agreco <greco031@umn.edu>
2023-03-25 19:25:20 -0500

    data frame works now! Plot developed

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -4,12 +4,18 @@ library(tidyverse)
 library(jsonlite)
 
 # Data Import and Cleaning
-rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json")
-rstats_original_tbl <- rstats_list$data$children$data
+#rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json")
+#rstats_original_tbl <- rstats_list$data$children$data
+
+rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
+rstats_original_tbl <- data_frame(rstats_list$data$children, stringsAsFactors = FALSE)
+
 rstats_tbl <- tibble(rstats_original_tbl) %>%
-  mutate(post = title) %>%
-  mutate(upvotes = ups) %>%
-  mutate(comments = num_comments) %>%
+  mutate(post = data.title) %>%
+  mutate(upvotes = data.ups) %>%
+  mutate(comments = data.num_comments) %>%
   select(c(post, upvotes, comments))
 
-ggplot(rstats_tbl, aes(x = upvotes, y = comments))
+ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
